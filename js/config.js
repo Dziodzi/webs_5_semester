99ba977e38eddf696207b24b80f642fc7f776304
Gregory <gsh812@inbox.ru>
2022-12-19 16:09:19 +0300

    fixed: config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('Page has been loaded successfully');
-});
+document.addEventListener('DOMContentLoaded',
+    event => handleLocationChange(document.location.href, '#dfff0e'));
+
 window.addEventListener('load', event => showLoadingTime());
