3f7011aa2d9c5ef79bc649788f9f7883067d81e9
Gregory <gsh812@inbox.ru>
2022-12-19 06:27:03 +0300

    impl: lab-6

---
+++ js/showReviews.js
@@ -0,0 +1,55 @@
+async function loadReviews() {
+    clearReviewsSection();
+    showLoadingIndicator();
+
+    try {
+        const randomNumber = Math.floor(Math.random() * 100 + 1);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${randomNumber}/comments`);
+        const comments = await response.json();
+
+        addReviews(comments);
+    } catch(error) {
+        alert("Oops! Something went wrong.");
+    }
+
+    hideLoadingIndicator();
+}
+
+function showLoadingIndicator() {
+    const loadingSection = document.getElementById('preloader');
+    loadingSection.innerHTML = '<img src="assets/preloader.gif" alt="Loading">';
+}
+
+function hideLoadingIndicator() {
+    const loadingSection = document.getElementById('preloader');
+    loadingSection.innerHTML = '';
+}
+
+function addReviews(comments) {
+    const reviewTemplate = document.querySelector('#reviewItem');
+    const reviewsSection = document.getElementById('reviews');
+
+    comments.forEach(comment => {
+        const reviewItem = reviewTemplate.content.cloneNode(true);
+
+        const name = reviewItem.querySelector('div h4');
+        const text = reviewItem.querySelector('div p');
+
+        name.innerHTML = comment.name;
+        text.innerHTML = comment.body;
+
+        reviewsSection.appendChild(reviewItem);
+    });
+}
+
+function clearReviewsSection() {
+    const reviewsSection = document.getElementById('reviews');
+    reviewsSection.innerHTML = '';
+}
+
+function getReviewsButtonOnClickHandler() {
+    const getReviewsButton = document.getElementById('getReviewsButton');
+    getReviewsButton.addEventListener('click', event => loadReviews());
+}
+
+document.addEventListener('DOMContentLoaded', event => getReviewsButtonOnClickHandler())
